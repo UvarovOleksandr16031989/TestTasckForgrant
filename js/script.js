3d348df6a054aeaf7ee7e5ea9888eb5fc4a9f705
UvarovOleksandr16031989 <alexuvarov1989@ukr.net>
2018-07-07 17:05:27 +0300

    Update 2018-07-07T14:05:09.569Z

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-var elem = document.getElementById('select');
-
-var i = 1950;
-while (i < 2018) {
-
-    elem.innerHTML += `<option class="section_form__input_box__select__content">${i}<option>`;
-
-    i++;
-}
-$(window).load(function() {
-    var images = $('img');
-    images.each(function(i) {
-        $(this).width($(this).width() / 2);
-    });
-});
+for(var elem=document.getElementById("select"),n=1950;n<2018;)elem.innerHTML+='<option class="section_form__input_box__select__content">'+n+"</option>",n++;$(window).load(function(){var n=$("img");n.each(function(n){$(this).width($(this).width()/2)})});
